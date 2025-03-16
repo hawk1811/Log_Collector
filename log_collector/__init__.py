2dbcb59b16e05c1bbfbd65565f15036751ab500a
The Hawk <the.hawk1811@gmail.com>
2025-03-16 21:16:26 +0200

    Delete log_collector directory

--- log_collector/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Log Collector - High-performance log collection and processing system.
-"""
-from .utils import get_version
-from .cli_main import CLI
-
-__version__ = get_version()
-__all__ = ['CLI', 'get_version']

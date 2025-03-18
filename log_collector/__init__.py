b9d2f5883081fd5b2b27277d3661951862d9dfc5
The Hawk <the.hawk1811@gmail.com>
2025-03-18 20:47:00 +0200

    Update __init__.py

--- log_collector/__init__.py
+++ log_collector/__init__.py
@@ -3,6 +3,7 @@ Log Collector - High-performance log collection and processing system.
 """
 from .utils import get_version
 from .cli_main import CLI
+from .auth import AuthManager
 
 __version__ = get_version()
-__all__ = ['CLI', 'get_version']
+__all__ = ['CLI', 'get_version', 'AuthManager']

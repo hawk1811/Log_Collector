2b655d79c27d4de1629d37130274a1b24af32194
The Hawk <the.hawk1811@gmail.com>
2025-03-19 20:05:21 +0200

    Update __init__.py

--- log_collector/__init__.py
+++ log_collector/__init__.py
@@ -1,5 +1,5 @@
 """
-Log Collector - High-performance log collection and processing system.
+Log Collector - High-performance log collection and processing system test.
 """
 from .utils import get_version
 from .cli_main import CLI

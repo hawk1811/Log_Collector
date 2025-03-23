7b7dc0a5e23bf2025831663625e992fe9ded7423
The Hawk <the.hawk1811@gmail.com>
2025-03-23 13:26:45 +0200

    Delete start_service.sh

--- start_service.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting LogCollector service..."
-./LogCollector --service start
-echo ""
-echo "Service startup initiated. Check logs for details."
-read -p "Press Enter to continue..."

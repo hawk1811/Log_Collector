7987e62a6b0b211eb0de4b4c31d7607c0bc65a4a
The Hawk <the.hawk1811@gmail.com>
2025-03-23 13:27:18 +0200

    Delete stop_service.sh

--- stop_service.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Stopping LogCollector service..."
-./LogCollector --service stop
-echo ""
-echo "Service stop command sent. Check logs for details."
-read -p "Press Enter to continue..."
